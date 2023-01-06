8d9a5ce4459df2ba2836cd170927755a9a3a5b09
barrosol <lucas.barroso@impactzero.pt>
2023-01-06 11:37:54 +0000

    Initial Commit

---
+++ navigationpoc/navigationpocApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationpocApp.swift
+//  navigationpoc
+//
+//  Created by Lucas Barroso IZ on 06/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct navigationpocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
